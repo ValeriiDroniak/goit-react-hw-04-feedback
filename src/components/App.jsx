da63be3fba8aac4c324ba88353d1de27d3db1993
Valerii Droniak <droniak.valerii@gmail.com>
2023-01-02 17:26:28 +0200

    refactoring App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { ImHappy2, ImNeutral2, ImSad2 } from 'react-icons/im';
 import Box from './Box/Box';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
@@ -12,58 +12,107 @@ const OPTIONS = {
   bad: ImSad2,
 };
 
-export class App extends Component {
-  state = {
+export const App = () => {
+  const [{ good, neutral, bad }, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  const countTotalFeedback = () => good + neutral + bad;
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100);
   };
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
+  const onLeaveFeedback = name => {
+    setState(prevState => ({ ...prevState, [name]: prevState[name] + 1 }));
   };
 
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
-  };
+  return (
+    <Box
+      display="flex"
+      flexWrap="wrap"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      padding={3}
+    >
+      <Section title="Please leave feedback" bg="rgb(243, 246, 249)">
+        <FeedbackOptions options={OPTIONS} onLeaveFeedback={onLeaveFeedback} />
+      </Section>
 
-  onLeaveFeedback = name => {
-    this.setState(prevState => ({ [name]: prevState[name] + 1 }));
-  };
-
-  render() {
-    const { bad, neutral, good } = this.state;
-    return (
-      <Box
-        display="flex"
-        flexWrap="wrap"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        padding={3}
-      >
-        <Section title="Please leave feedback" bg="rgb(243, 246, 249)">
-          <FeedbackOptions
-            options={OPTIONS}
-            onLeaveFeedback={this.onLeaveFeedback}
+      <Section title="Statistics">
+        {bad > 0 || neutral > 0 || good > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
+        ) : (
+          <Notification message="There is no any feedback" />
+        )}
+      </Section>
+    </Box>
+  );
+};
 
-        <Section title="Statistics">
-          {bad > 0 || neutral > 0 || good > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message="There is no any feedback" />
-          )}
-        </Section>
-      </Box>
-    );
-  }
-}
+// export const App = () => {
+//   const [good, setGood] = useState(0);
+//   const [neutral, setNeutral] = useState(0);
+//   const [bad, setBad] = useState(0);
+
+//   const countTotalFeedback = () => good + neutral + bad;
+
+//   const countPositiveFeedbackPercentage = () => {
+//     return Math.round((good / countTotalFeedback()) * 100);
+//   };
+
+//   const onLeaveFeedback = name => {
+//     switch (name) {
+//       case 'good':
+//         setGood(preGood => preGood + 1);
+//         break;
+//       case 'neutral':
+//         setNeutral(preNeutral => preNeutral + 1);
+//         break;
+//       case 'bad':
+//         setBad(preBad => preBad + 1);
+//         break;
+
+//       default:
+//         return;
+//     }
+//   };
+
+//   return (
+//     <Box
+//       display="flex"
+//       flexWrap="wrap"
+//       flexDirection="column"
+//       justifyContent="center"
+//       alignItems="center"
+//       padding={3}
+//     >
+//       <Section title="Please leave feedback" bg="rgb(243, 246, 249)">
+//         <FeedbackOptions options={OPTIONS} onLeaveFeedback={onLeaveFeedback} />
+//       </Section>
+
+//       <Section title="Statistics">
+//         {bad > 0 || neutral > 0 || good > 0 ? (
+//           <Statistics
+//             good={good}
+//             neutral={neutral}
+//             bad={bad}
+//             total={countTotalFeedback()}
+//             positivePercentage={countPositiveFeedbackPercentage()}
+//           />
+//         ) : (
+//           <Notification message="There is no any feedback" />
+//         )}
+//       </Section>
+//     </Box>
+//   );
+// };
